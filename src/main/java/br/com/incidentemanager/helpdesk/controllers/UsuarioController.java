0bacfd91a36c1931769dac759a90ab9203920669
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-10-07 23:19:20 -0300

    Adicionando UsuarioController

---
+++ src/main/java/br/com/incidentemanager/helpdesk/controllers/UsuarioController.java
@@ -0,0 +1,19 @@
+package br.com.incidentemanager.helpdesk.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.incidentemanager.helpdesk.configs.ControllerConfig;
+import br.com.incidentemanager.helpdesk.services.UsuarioService;
+
+@RestController
+@RequestMapping(ControllerConfig.PRE_URL + "/usuario")
+@CrossOrigin(origins = { "http://localhost", "http://localhost:4200", "http://localhost:4200/*" })
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+
+}
