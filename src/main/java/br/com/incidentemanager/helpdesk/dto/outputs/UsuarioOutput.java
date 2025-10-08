28e8ac9630c7732f9b1b7182ac5e53263bb8bf4e
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-10-07 22:41:08 -0300

    Adicionando dto UsuarioOutput

---
+++ src/main/java/br/com/incidentemanager/helpdesk/dto/outputs/UsuarioOutput.java
@@ -0,0 +1,19 @@
+package br.com.incidentemanager.helpdesk.dto.outputs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioOutput {
+
+	private Long id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String telefone;
+	
+	private boolean ativo;
+}
