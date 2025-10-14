c61a52f75c1a1722075e696d4987934a2dd86243
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-10-14 16:04:36 -0300

    Adicionando novas entidades

---
+++ src/main/java/br/com/incidentemanager/helpdesk/entities/LayoutEmailEntity.java
@@ -0,0 +1,39 @@
+package br.com.incidentemanager.helpdesk.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "layouts_email")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LayoutEmailEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "source_email")
+	private String sourceEmail;
+
+	@Column(name = "subject")
+	private String subject;
+
+	@Column(name = "body", length = 4000)
+	private String body;
+
+}
---
+++ src/main/java/br/com/incidentemanager/helpdesk/entities/RedefiniSenhaEntity.java
@@ -0,0 +1,53 @@
+package br.com.incidentemanager.helpdesk.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Index;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "redefini_senhas", indexes = { @Index(name = "idx_usuario_id", columnList = "user_id"),
+		@Index(name = "idx_hash", columnList = "hash") })
+@Getter
+@Setter
+public class RedefiniSenhaEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "hash", unique = true, nullable = false, length = 100)
+	private String hash;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private UsuarioEntity usuario;
+
+	@Column(name = "expiration_time", nullable = false)
+	private LocalDateTime expirationTime;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+
+	@Column(name = "used_at")
+	private LocalDateTime usedAt;
+
+	@Column(name = "used", nullable = false)
+	private boolean used = false;
+	
+	@PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
