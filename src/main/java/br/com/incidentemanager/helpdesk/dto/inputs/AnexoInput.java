677e15e26e51231129265807bcce32e2937dfed2
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-10-28 22:26:04 -0300

    Adicionando inputs de chamado

---
+++ src/main/java/br/com/incidentemanager/helpdesk/dto/inputs/AnexoInput.java
@@ -0,0 +1,31 @@
+package br.com.incidentemanager.helpdesk.dto.inputs;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.incidentemanager.helpdesk.enums.TipoAnexoEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnexoInput {
+
+	@NotBlank(message = "O nome do arquivo é obrigatório.")
+    @Size(max = 255, message = "O nome do arquivo não pode exceder {max} caracteres.")
+    private String nomeArquivo;
+	
+    @NotNull(message = "O tamanho do arquivo é obrigatório.")
+    @Positive(message = "O tamanho do arquivo deve ser positivo.")
+    private Long tamanhoBytes;
+    
+    @NotNull(message = "O tipo do anexo é obrigatório.")
+    private TipoAnexoEnum tipo;
+    
+    @NotNull(message = "O arquivo é obrigatório.")
+    private MultipartFile arquivo;
+	
+}
---
+++ src/main/java/br/com/incidentemanager/helpdesk/dto/inputs/ChamadoInput.java
@@ -0,0 +1,30 @@
+package br.com.incidentemanager.helpdesk.dto.inputs;
+
+import java.util.List;
+
+import br.com.incidentemanager.helpdesk.enums.PrioridadeEnum;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChamadoInput {
+
+	@NotBlank(message = "O título do chamado é obrigatório.")
+    @Size(min = 5, max = 100, message = "O título deve conter entre {min} e {max} caracteres.")
+    private String titulo;
+	
+    @NotBlank(message = "A descrição do chamado é obrigatória.")
+    @Size(min = 10, max = 2000, message = "A descrição deve conter entre {min} e {max} caracteres.")
+    private String descricao;
+
+    @NotNull(message = "O nível de prioridade é obrigatório.")
+    private PrioridadeEnum prioridade;
+    
+    @Valid
+    private List<AnexoInput> anexos;
+}
