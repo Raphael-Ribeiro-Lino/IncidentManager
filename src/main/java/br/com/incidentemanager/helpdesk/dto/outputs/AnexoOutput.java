665d3815444a161fc8339e49af29a60817ef20a5
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-11-19 14:23:11 -0300

    Alterando anexo output

--- src/main/java/br/com/incidentemanager/helpdesk/dto/outputs/AnexoOutput.java
+++ src/main/java/br/com/incidentemanager/helpdesk/dto/outputs/AnexoOutput.java
@@ -2,8 +2,6 @@ package br.com.incidentemanager.helpdesk.dto.outputs;
 
 import java.time.Instant;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import br.com.incidentemanager.helpdesk.enums.TipoAnexoEnum;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,7 +16,6 @@ public class AnexoOutput {
     
     private TipoAnexoEnum tipo;
     
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss", timezone = "America/Sao_Paulo")
     private Instant uploadedAt;
     
     private UsuarioOutput enviadoPor;
