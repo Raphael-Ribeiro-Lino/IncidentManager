66f1a2738f7eb79b9376d4d7176d6311b12acc7f
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-11-20 20:40:50 -0300

    Adicionando entidade transferencias

---
+++ src/main/java/br/com/incidentemanager/helpdesk/entities/TransferenciaEntity.java
@@ -0,0 +1,63 @@
+package br.com.incidentemanager.helpdesk.entities;
+
+import java.time.Instant;
+
+import br.com.incidentemanager.helpdesk.enums.StatusTransferenciaEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "transferencias")
+@Getter
+@Setter
+public class TransferenciaEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "chamado_id", nullable = false)
+	private ChamadoEntity chamado;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "tecnico_origem_id", nullable = false)
+	private UsuarioEntity tecnicoOrigem; // Quem solicitou a transferência
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "tecnico_destino_id", nullable = false)
+	private UsuarioEntity tecnicoDestino; // Quem deve aceitar
+
+	@Column(name = "motivo", nullable = false)
+	private String motivo;
+
+	@Column(name = "motivo_recusa")
+	private String motivoRecusa;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status", nullable = false)
+	private StatusTransferenciaEnum status;
+
+	@Column(name = "dataSolicitacao", nullable = false)
+	private Instant dataSolicitacao;
+
+	@Column(name = "dataResposta")
+	private Instant dataResposta;
+
+	@PrePersist
+	public void prePersist() {
+		this.dataSolicitacao = Instant.now();
+		this.status = StatusTransferenciaEnum.PENDENTE;
+	}
+}
